e04e3bb548812aa8ca4fb3469aa38e5c0b337f3a
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-12 23:33:53 +0530

    Create challengeindex.js

---
+++ Object Literals/For of Loop/challengeindex.js
@@ -0,0 +1,15 @@
+// Challenge - For Of Loop
+
+// Using the For of Loop, iterate through the array and print into the console, a message like:
+// Tom lives in Lisbon
+
+const students = [ 
+    { name: "John", city: "New York" },
+    { name: "Peter", city: "Paris"},
+    { name: "Kate", city: "Sidney" }
+];
+
+
+for( const student of students ) {
+    console.log( student.name + " lives in " + student.city );
+}
