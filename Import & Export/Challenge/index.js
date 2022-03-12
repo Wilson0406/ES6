245764723643dd52c23616e6b5b3a4c7d19692e6
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-13 00:11:36 +0530

    Update index.js

--- Import & Export/Challenge/index.js
+++ Import & Export/Challenge/index.js
@@ -1,3 +1,17 @@
+/*
+
+**Challenge**
+
+Inside of the file data.js, create a function add, that will receive 2 numbers and return the sum of them.
+Make sure to export this function.
+
+- Import the function add, into the index.js file
+- Create a variable result, that will hold the result of the function add when you call it and pass 2 numbers into it.
+- print into the console the value of the variable result;
+
+*/
+
+
 import { add } from './data.js';
 
 let result = add(3, 2);
