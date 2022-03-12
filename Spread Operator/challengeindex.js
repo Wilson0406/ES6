541cf8e53690d0551679ce94194ab0a700e56323
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-12 23:45:28 +0530

    Create challengeindex.js

---
+++ Spread Operator/challengeindex.js
@@ -0,0 +1,18 @@
+/*
+    **** Challenge ****
+    
+    Imagine you are going out to do some grocery shopping.
+    So you have an array called shoppingList with all the products you want to buy.
+    
+    Now that you are inside of the shop, you have a basket with all the products from your list, but you want to add a few more.
+    
+    Create a new array called shoppingBasket, that will be a copy of the shoppingList array, and add some new products into it. 
+
+*/
+
+const shoppingList = ["eggs", "milk", "butter"];
+
+
+const shoppingBasket = [ ...shoppingList, "bread", "pasta"];
+
+console.log(shoppingBasket);
