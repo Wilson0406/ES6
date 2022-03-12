d49149a06f7b2b3dc8f2bb10a2c1ff0d007d2d57
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-12 23:54:31 +0530

    Create index.js

---
+++ Default Params/index.js
@@ -0,0 +1,5 @@
+const leadSinger = (artist = "someone") => {          //someone is default if no argument is provided
+    console.log(`${artist} is the lead singer of Cold Play`);
+}
+
+leadSinger("Chris Martin");
