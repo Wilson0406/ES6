7f15d477306147ebf65a3e932a666d7e0a845680
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-13 00:15:18 +0530

    Create index.js

---
+++ padStart() & padEnd()/index.js
@@ -0,0 +1,17 @@
+// let example = 'Dylan';
+
+
+
+// console.log(example.padStart(10, 'a'));
+
+// let example = 'Dylan Israel';
+
+
+
+// console.log(example.padStart(10, 'a'));
+
+let example = 'Dylan';
+
+
+
+console.log(example.padEnd(10, 'a'));
