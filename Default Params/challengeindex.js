d579b5b17e24898777a02aae53e7d8761488384e
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-12 23:55:37 +0530

    Create challengeindex.js

---
+++ Default Params/challengeindex.js
@@ -0,0 +1,19 @@
+/*
+**** Challenge *****
+
+Create a function that receives a parameter of food.
+If your parameter food is going to have a value of "milk"
+the function should print into the console the following:
+
+"I'm going to buy milk from the grocery shop"
+
+But if you dont pass a value to your parameter food, it should print
+"I'm going to buy something from the grocery shop"
+
+*/
+
+function foodShopping( food = "something") {
+    console.log(`I'm going to buy ${food} from the grocery shop`);
+}
+
+foodShopping();
